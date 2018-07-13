28bf33c343149a53f7633eed096ed8caf0a080cf
HyelimKim0716 <hayley7927@gmail.com>
2018-07-13 23:18:19 +0900

    Make base classes

---
+++ app/src/main/kotlIn/com/riiid/infiniteposts/riiidpostlist/base/BaseBindingActivity.kt
@@ -0,0 +1,2 @@
+package com.riiid.infiniteposts.riiidpostlist.base
+
---
+++ app/src/main/kotlIn/com/riiid/infiniteposts/riiidpostlist/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.riiid.infiniteposts.riiidpostlist.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.riiid.infiniteposts.riiidpostlist.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
